da7722a78ce79a7e5a0a76c7a695ec6d404d7dad
Chris <chriswolfdesign@gmail.com>
2020-06-10 11:19:47 -0400

    Refactored Maze.py

--- src/model/Maze.py
+++ src/model/Maze.py
@@ -21,7 +21,6 @@ class Maze:
     def __init__(self, file_name):
         """
         Constructor
-
         :param file_name: the text file we are reading the maze from
         """
         self._file_name = file_name
@@ -30,7 +29,6 @@ class Maze:
     def _get_width(self):
         """
         Gets the width of the maze
-
         :return: the width of the maze
         """
         return len(self._maze[0])
@@ -38,15 +36,20 @@ class Maze:
     def _get_height(self):
         """
         Gets the height of the maze
-
         :return: the height of the maze
         """
         return len(self._maze)
 
+    def go(self):
+        """
+        Starts the behavior of the maze
+        """
+        self._draw_image()
+        self._print_maze()
+
     def _generate_maze(self):
         """
         Reads the given text file and converts it to a 2D array
-
         :return: the 2D array representation of the maze
         """
         # if we cannot find the file, give up
@@ -61,7 +64,6 @@ class Maze:
 
         # Ensure that the maze is valid
         self._validate_maze(characters)
-        characters = characters.splitlines()
         self._create_maze_array(characters)
 
         # close the file
@@ -72,6 +74,7 @@ class Maze:
         Generates the maze based on the characters array passed in
         :param characters: the array of characters to convert into a maze
         """
+        characters = characters.splitlines()
         maze = []
         # create the 2D array
         for line in characters:
@@ -96,23 +99,23 @@ class Maze:
             print('ERROR -- File must have exactly one goal!')
             exit(1)
 
-    def print_maze(self):
+    def _print_maze(self):
         """
         Prints the maze to the terminal
         """
-        if self._maze is None:
-            print('No maze has been loaded.')
-            return
+        self._quit_if_no_valid_maze()
 
         for line in self._maze:
             for character in line:
                 print(character, end='')
             print()  # for new line
 
-    def draw_image(self):
+    def _draw_image(self):
         """
         Draws the maze to a png file to ease of viewing
         """
+        self._quit_if_no_valid_maze()
+
         cell_size = 50
         cell_border = 2
 
@@ -120,19 +123,36 @@ class Maze:
                           'black')
         draw = ImageDraw.Draw(image)
 
+        self._generate_image_from_characters(cell_border, cell_size, draw)
+        self._save_image_to_file(image)
+
+    def _save_image_to_file(self, image):
+        """
+        Saves the image as png file to the maze_images directory
+        :param image: the image that should be saved
+        """
+        file_name_without_path = self._file_name.split('/')[-1]
+        image_file_name = '../../maze_images/' + file_name_without_path[:-4] + '.png'
+        image.save(image_file_name)
+
+    def _generate_image_from_characters(self, cell_border, cell_size, draw):
+        """
+        Generates the image to be saved from the maze arrary
+        :param cell_border: the amount of space between each tile
+        :param cell_size: the number of pixels each tile should be
+        :param draw: the object responsible for drawing to the image
+        """
         for i in range(self._get_height()):
             for j in range(self._get_width()):
+                # determine the color of the tile
                 fill = self._get_tile_color(self._maze[i][j])
 
+                # draw the tile to the image
                 draw.rectangle(
                     ([(j * cell_size + cell_border, i * cell_size + cell_border),
                       ((j + 1) * cell_size - cell_border, (i + 1) * cell_size - cell_border)]),
                     fill=fill
                 )
-
-        file_name_without_path = self._file_name.split('/')[-1]
-        image_file_name = '../../maze_images/' + file_name_without_path[:-4] + '.png'
-        image.save(image_file_name)
 
     def _get_tile_color(self, character):
         """
@@ -150,9 +170,10 @@ class Maze:
         else:
             return 0, 0, 0  # black
 
-    def go(self):
+    def _quit_if_no_valid_maze(self):
         """
-        Starts the behavior of the maze
+        If there is no appropriate maze value, exit the program
         """
-        self.draw_image()
-        self.print_maze()
+        if self._maze is None:
+            print('No maze has been loaded.')
+            exit(1)
