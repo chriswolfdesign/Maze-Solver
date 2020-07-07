a02d84b11a621e851545459fdca91a400b61e6a7
Chris <chriswolfdesign@gmail.com>
2020-07-07 09:28:26 -0400

    Program can now be ran as a module with no command line arguments

---
+++ MazeSolver/__main__.py
@@ -0,0 +1,13 @@
+from MazeSolver.app import run
+
+"""
+__main__.py
+
+Python conventional file to allow running the program as a module
+
+Author: Chris Wolf
+Version: 1.0.0 (July 7, 2020)
+"""
+
+if __name__ == '__main__':
+    run()
---
+++ MazeSolver/factories/frontier_factory.py
@@ -0,0 +1,22 @@
+from MazeSolver.model.frontiers.queue_frontier import QueueFrontier
+from MazeSolver.model.frontiers.stack_frontier import StackFrontier
+from MazeSolver.enums.frontier_choices import *
+
+"""
+FrontierFactory.py
+
+Holds the methods necessary to generate the correct frontier type
+
+Author: Chris Wolf
+Version: 1.0.0 (June 30, 2020)
+"""
+
+
+def generate_frontier(frontier_choice):
+    if frontier_choice == STACK_FRONTIER:
+        return StackFrontier()
+    elif frontier_choice == QUEUE_FRONTIER:
+        return QueueFrontier()
+    else:
+        print('ERROR -- Invalid frontier!')
+        exit()
